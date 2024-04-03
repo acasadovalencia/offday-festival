c0b7cfdd57330192a63952148109ecdc2e23a723
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-04-04 00:09:42 +0200

    Marquee JS done

--- js/index.js
+++ js/index.js
@@ -2,16 +2,20 @@
 
 const lineupLis = document.querySelectorAll('.Lineup-li')
 const lineupImgs = document.querySelectorAll('.Lineup-img')
+const marqueeUl = document.querySelector('.Marquee-ul')
 
 lineupLis.forEach( (each , i)=>{
-    lineupLis[i].addEventListener('mouseover' , function(){
+    lineupLis[i].addEventListener('mouseover' , ()=>{
         lineupLis[i].classList.add('isActive')
         lineupImgs[i].classList.add('isActive')
     })
 
-    lineupLis[i].addEventListener('mouseout' , function(){
+    lineupLis[i].addEventListener('mouseout' , ()=>{
         lineupLis[i].classList.remove('isActive')
         lineupImgs[i].classList.remove('isActive')
 
     })
 })
+
+marqueeUl.addEventListener('mouseover' , ()=> marqueeUl.classList.remove('isMoving'))
+marqueeUl.addEventListener('mouseout' , ()=> marqueeUl.classList.add('isMoving'))
