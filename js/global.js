ee1330e036a4c7d42af6ad92dbd9ba71680b5a33
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-04-06 21:14:37 +0200

    Fixed transitions delays

--- js/global.js
+++ js/global.js
@@ -9,14 +9,22 @@ const navUl = document.querySelector('.Nav-ul')
 menuBtn.addEventListener('click' ,() => {
     menuBtn.classList.remove('isActive')
     btnTickets.classList.remove('isActive')
+    navUl.classList.remove('navUp')
     headerNav.classList.add('isOpen')
     btnClose.classList.add('isActive')
-    navUl.classList.add('isActive')
+    setTimeout(() => {
+        navUl.classList.remove('navDown')
+        navUl.classList.add('navUp')
+    }, 1000);
 })
 btnClose.addEventListener('click' , () => {
-    menuBtn.classList.add('isActive')
-    btnTickets.classList.add('isActive')
-    headerNav.classList.remove('isOpen')
-    btnClose.classList.remove('isActive')
-    navUl.classList.remove('isActive')
+    setTimeout(() => {
+        headerNav.classList.remove('isOpen')
+        menuBtn.classList.add('isActive')
+        btnTickets.classList.add('isActive')
+        btnClose.classList.remove('isActive')
+    }, 800);
+    navUl.classList.remove('navUp')
+    navUl.classList.remove('navDown')
+    navUl.classList.add('navDown')
 })
