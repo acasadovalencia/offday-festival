39ba2d25a2788e26c6d6b36db8fa6c349489d542
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-04-13 19:19:11 +0200

    Menu animation changed in index

--- js/global.js
+++ js/global.js
@@ -4,18 +4,27 @@ const menuBtn = document.querySelector('.Menu-btn')
 const btnClose = document.querySelector('.Btn-close')
 const headerNav = document.querySelector('.Header-nav')
 const btnTickets = document.querySelector('.Btn-tickets')
-const navUl = document.querySelector('.Nav-ul')
+// const navUl = document.querySelector('.Nav-ul')
+const navLis = document.querySelectorAll('.Nav-li')
 
 menuBtn.addEventListener('click' ,() => {
     menuBtn.classList.remove('isActive')
     btnTickets.classList.remove('isActive')
-    navUl.classList.remove('navUp')
+    // navUl.classList.remove('navUp')
     headerNav.classList.add('isOpen')
     btnClose.classList.add('isActive')
     setTimeout(() => {
-        navUl.classList.remove('navDown')
-        navUl.classList.add('navUp')
-    }, 1000);
+        navLis.forEach( (each , i) => {
+            navLis[i].classList.remove('navDown')
+            navLis[i].classList.add('navUp')
+        })
+        navLis.forEach((each , i) => {
+            navLis[i].classList.remove('navDown')
+        })
+        navLis.forEach((each , i) => {
+            navLis[i].classList.add('navUp')
+        })
+    }, 800)
 })
 btnClose.addEventListener('click' , () => {
     setTimeout(() => {
@@ -23,8 +32,11 @@ btnClose.addEventListener('click' , () => {
         menuBtn.classList.add('isActive')
         btnTickets.classList.add('isActive')
         btnClose.classList.remove('isActive')
-    }, 800);
-    navUl.classList.remove('navUp')
-    navUl.classList.remove('navDown')
-    navUl.classList.add('navDown')
-})
+    }, 800)
+    navLis.forEach((each , i) => {
+        navLis[i].classList.remove('navUp')
+        navLis[i].classList.remove('navDown')
+        navLis[i].classList.add('navDown')
+
+    }, 800)
+}) 
